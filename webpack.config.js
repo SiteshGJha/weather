982a1b6f24dec1f0fc927c3e9fcf0d85915cab25
Sitesh Jha <siteshgjha@gmail.com>
2019-11-23 01:23:13 +0530

    Add sass packages and sass related webpack config. added initial app.js file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: isDevelopment ? 'development' : 'production',
@@ -18,17 +19,54 @@ module.exports = {
                 test: /\.html$/,
                 loader: 'html-loader',
                 options: { minimize: !isDevelopment }
+            },
+            {
+                test: /\.style\.s(a|c)ss$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            }, 
+            {
+                test: /\.s(a|c)ss$/,
+                exclude: /\.style\.(s(a|c)ss)$/,
+                loader:[
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
             }
         ]
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx', '.scss']
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebPackPlugin({
             template: './src/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
         })
     ]
 }
