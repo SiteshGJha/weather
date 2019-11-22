b4584b45643b31d2fcb2d01caf96a0a3c5567b37
Sitesh Jha <siteshgjha@gmail.com>
2019-11-23 01:23:33 +0530

    Add index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Weather App");
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+import './global'
+
+ReactDOM.render(<App />, document.getElementById('root'));
